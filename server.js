dfc042c4cbfaf8a0c6cfa11636e8e77d654ceeeb
Robert Masters <rob.masters@pebaro.co.uk>
2022-05-30 09:42:55 +0100

    test for Postman app

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const app = express()
 
+app.get('/', (req, res) => res.send('Testing In Postman App'))
+
 const PORT = process.env.PORT || 5001
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`))
