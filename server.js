275d38a636dc76fe1c82c57bfefccfd04482a7e3
Robert Masters <rob.masters@pebaro.co.uk>
2022-05-30 09:54:16 +0100

    set up basic routes for initial testing

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => res.json({
 	msg: 'Welcome to the Contact Keeper Tech Test API for Dotcrafted'
 }))
 
+// DEFINE ROUTES
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/contacts', require('./routes/contacts'))
+app.use('/api/users', require('./routes/users'))
+
 const PORT = process.env.PORT || 5001
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`))
