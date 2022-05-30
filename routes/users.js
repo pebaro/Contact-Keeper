6d77fa45f7f4166702c07e6f22a5380ccfc3a267
Robert Masters <rob.masters@pebaro.co.uk>
2022-05-30 11:34:18 +0100

    jwt working, creating middleware

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
-const bcrypt = require('bcryptjs')
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const config = require('config')
 const { check, validationResult } = require('express-validator')
