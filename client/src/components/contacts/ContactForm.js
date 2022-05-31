4d2590959e72597312cc7942d6ec1965b7ab6d77
Robert Masters <rob.masters@pebaro.co.uk>
2022-05-31 16:16:40 +0100

    clear current to clear the form

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -4,7 +4,7 @@ import ContactContext from '../../context/contact/contactContext'
 const ContactForm = () => {
 	const contactContext = useContext(ContactContext)
 
-	const { addContact, current } = contactContext
+	const { addContact, current, clearCurrent } = contactContext
 
 	useEffect(() => {
 		if (current !== null) {
@@ -46,9 +46,15 @@ const ContactForm = () => {
 		})
 	}
 
+	const clearAll = () => {
+		clearCurrent()
+	}
+
 	return (
 		<form onSubmit={onSubmit}>
-			<h2 className="text-primary">Add Contact</h2>
+			<h2 className="text-primary">
+				{current ? 'Edit Contact' : 'Add Contact'}
+			</h2>
 			<input
 				type="text"
 				placeholder="Name"
@@ -92,10 +98,18 @@ const ContactForm = () => {
 			<div>
 				<input
 					type="submit"
-					value="Add Contact"
+					value={current ? 'Update Contact' : 'Add Contact'}
 					className="btn btn-primary btn-block"
 				/>
 			</div>
+			{
+				current && <div>
+					<button
+						className="btn btn-light btn-block"
+						onClick={clearAll}
+					>Clear</button>
+				</div>
+			}
 		</form>
 	)
 }
