beb2b2a3bd69b9dc690dbf43c402fe4423d7f2c0
Robert Masters <rob.masters@pebaro.co.uk>
2022-05-31 08:34:24 +0100

    created folder and files in wrong location so it's now moved to correct location

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Home from './components/pages/home'
 import About from './components/pages/about'
+
+import ContactState from './context/contact/ContactState'
 import './App.css';
 
 const App = () => {
