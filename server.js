89667a99d3c72479f868875de290d20408770e41
Robert Masters <rob.masters@pebaro.co.uk>
2022-06-01 11:25:07 +0100

    last bits before deployment to Heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const connectDB = require('./config/db')
+const path = require('path')
 
 const app = express()
 
@@ -19,6 +20,16 @@ app.get('/', (req, res) => res.json({
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/contacts', require('./routes/contacts'))
 app.use('/api/users', require('./routes/users'))
+
+// serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+	// set static folder
+	app.use(express.static('client/build'))
+
+	app.get(
+		'*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	)
+}
 
 const PORT = process.env.PORT || 5001
 
