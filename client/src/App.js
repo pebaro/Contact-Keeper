4b4deba8818276ad3e87bb948d6aa811a1ed56d9
Robert Masters <rob.masters@pebaro.co.uk>
2022-06-01 09:15:09 +0100

    homepage now private when logged out

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import About from './components/pages/about'
 import Register from './components/auth/Register'
 import Login from './components/auth/Login'
 import Alerts from './components/layout/Alerts'
+import PrivateRoute from './components/routing/PrivateRoute'
 
 import ContactState from './context/contact/ContactState'
 import AuthState from './context/auth/AuthState'
@@ -29,7 +30,7 @@ const App = () => {
 								<div className="container">
 									<Alerts />
 									<Switch>
-										<Route 
+										<PrivateRoute 
 											exact path='/' 
 											component={Home} 
 										/>
